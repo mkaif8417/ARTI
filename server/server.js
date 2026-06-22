11c509f9b941eee5e61f7c112f810030e430ec5a
mkaif8417 <mkaif6937@gmail.com>
2026-06-23 01:40:43 +0530

    second deploy

--- server/server.js
+++ server/server.js
@@ -1,12 +1,38 @@
+const dotenv = require('dotenv');
+dotenv.config(); // must be FIRST before any other imports
+
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/payments', paymentRoutes);
-
+app.use(notFound);
+app.use(errorHandler);
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.use(notFound);
-app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
