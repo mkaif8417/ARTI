a5acd664722ff6f03906a602666f29b654029e33
Mohd Kaif <mkaif6937@gmail.com>
2026-06-23 11:07:47 +0530

    Fixing  Admin operations from office pc

--- server/server.js
+++ server/server.js
@@ -24,11 +24,13 @@ app.use('/api/products', productRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/payments', paymentRoutes);
-app.use(notFound);
-app.use(errorHandler);
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
+
+app.use(notFound);
+app.use(errorHandler);
+
 
 
 const PORT = process.env.PORT || 5000;
